8369a9779348e5b7e53459f6c709b037fa9f4639
hasnatahmedchuhan <hasnatahmedchuhan@gmail.com>
2025-06-30 03:32:25 +0000

    Optimize for deployment

---
+++ assets/main.js
@@ -0,0 +1,30 @@
+// Add to very top of main.js
+const { app } = require('electron');
+app.disableHardwareAcceleration();
+process.env.ELECTRON_DISABLE_GPU = 'true';
+
+// Then the rest of your code
+const { BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
